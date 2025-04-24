b5501b2ecf9f5199c1ced8186b77488553f63ba3
soumyakarwa <soumyakarwa01@gmail.com>
2025-04-23 22:47:26 -0400

    added icons

--- src/lib/components/Globe.svelte
+++ src/lib/components/Globe.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	// CODE FOR DRAGGABLE GLOBE BUILT ON https://svelte.dev/playground/9617e79b67fb4f48a0f744f6714b62a3?version=4.1.1
 	import countries from '$lib/data/map.topojson.json';
-	import { geoOrthographic, geoPath, drag, select } from 'd3';
+	import ingredients from '$lib/data/ingredients.json';
+	import IngredientIcon2 from '$lib/components/IngredientIcon2.svelte';
+	import { getCoords } from './utils/getCoords';
+	import { geoOrthographic, geoPath, drag } from 'd3';
 	import { feature } from 'topojson-client';
 	import { onMount } from 'svelte';
 
@@ -56,6 +59,21 @@
 		animationFrame = requestAnimationFrame(update);
 	};
 
+	const isVisible = (coords: [number, number]): boolean => {
+		// const [x, y, z] = projection.rotate(); // get the rotation
+		const [lng, lat] = coords;
+
+		// convert both to radians
+		const lambda = lng * (Math.PI / 180);
+		const phi = lat * (Math.PI / 180);
+		const lambda0 = -rotation[0] * (Math.PI / 180);
+
+		// dot product check (z > 0 means facing forward)
+		const cosc =
+			Math.sin(phi) * Math.sin(0) + Math.cos(phi) * Math.cos(0) * Math.cos(lambda - lambda0);
+		return cosc > 0;
+	};
+
 	onMount(() => {
 		autoRotate();
 		// dragHandler(globe);
@@ -67,14 +85,22 @@
 	$effect(() => {});
 </script>
 
-<div class="flex w-full flex-row gap-2">
-	<div class="relative h-full w-1/2" bind:clientWidth={width}>
-		<svg {width} {height} preserveAspectRatio="xMidYMid meet">
-			<g class="origin-center" transform={`translate(${margin.left}, ${margin.top})`}>
-				<path bind:this={globe} d={path({ type: 'Sphere' })} fill="#9eeef1" stroke="#000" />
-				<path d={path(land)} fill="#fff6d8" stroke="#000" />
-			</g>
-		</svg>
+<div class="flex h-full w-full items-center justify-center">
+	<div class="flex w-full flex-row gap-2">
+		<div class="relative h-full w-1/2" bind:clientWidth={width}>
+			<svg {width} {height} preserveAspectRatio="xMidYMid meet">
+				<g class="origin-center" transform={`translate(${margin.left}, ${margin.top})`}>
+					<path bind:this={globe} d={path({ type: 'Sphere' })} fill="#9eeef1" stroke="#000" />
+					<path d={path(land)} fill="#fff6d8" stroke="#000" />
+					{#each ingredients as ingredient, i}
+						{@const lngLat = getCoords(ingredient.country.coords)}
+						{@const coords = projection(lngLat)}
+						{@const visible = isVisible(lngLat)}
+						<IngredientIcon2 {ingredient} {coords} {i} {visible} />
+					{/each}
+				</g>
+			</svg>
+		</div>
+		<div>INGREDIENT INFO</div>
 	</div>
-	<div>INGREDIENT INFO</div>
 </div>
